e7b7892993a7bba7fe58239063f5d228f4e93fff
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2024-12-08 06:15:11 +0530

    decryption done

--- constants/litAction.js
+++ constants/litAction.js
@@ -1,42 +1,17 @@
 // @ts-nocheck
 const _litActionCode = async () => {
   try {
+    
+
     const apiKey = await Lit.Actions.decryptAndCombine({
       accessControlConditions,
       ciphertext,
       dataToEncryptHash,
       authSig: null,
-      chain: "ethereum",
+      chain: "baseSepolia",
     });
 
-    const blockNumber = await Lit.Actions.runOnce(
-      { waitForResponse: true, name: "ETH block number" },
-      async () => {
-        const resp = await fetch(`https://eth-mainnet.g.alchemy.com/v2/${apiKey}`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            jsonrpc: "2.0",
-            id: 1,
-            method: "eth_blockNumber",
-            params: [],
-          }),
-        });
-
-        let data = await resp.json();
-
-        if (data.result) {
-          data.result = parseInt(data.result, 16);
-          return data.result;
-        } else {
-          throw new Error("Failed to get block number");
-        }
-      }
-    );
-
-    Lit.Actions.setResponse({ response: blockNumber });
+    Lit.Actions.setResponse({ response: apiKey });
   } catch (e) {
     Lit.Actions.setResponse({ response: e.message });
   }
